f5fe461294c8903110142691eb68a4514be6c04e
Andreas Hagele <andreas.hagele@gmail.com>
2023-11-28 07:20:25 +1300

    creates cvs

---
+++ GetData.py
@@ -0,0 +1,100 @@
+import sys
+import pickle
+import numpy as np
+from io import StringIO
+import sys
+import os
+
+try:
+    from Tkinter import Tk
+    from Tkinter.filedialog import askopenfilename
+    from Tkinter import messagebox
+except ImportError:
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+    from tkinter import messagebox
+
+class logToFile:
+    def __init__(self, baseName):
+        self.baseName = baseName
+        self.fileIndex = 1
+        self.fileName = ""
+        self.lines = []
+
+    def startLog(self):
+        self.fileName = 'cal{0}.txt'.format(self.fileIndex)
+
+    def setHeader(self, line):
+        self.header = 'Timestamp, HVRef,  HD755, HD1064,  HD532,  PWus,  Seed,   Amp,  ASE'
+
+    def addLog(self, line):
+        # add to list of strings
+        self.lines.append(line)
+
+    def endLog(self):
+        # ToDo
+        # format the output and write to file
+        # convert to table with numpy.loadtxt()
+        # reverse lines
+        # filter required lines
+        # save to file
+        self.lines.reverse()
+        allData = np.loadtxt(self.lines, delimiter=',', usecols = (1,11,12,13,14,16,18,19,20), dtype='str')
+        print(allData)
+        outName = '{0}_{1}.csv'.format(self.baseName, self.fileIndex)
+        print(f'{outName = }')
+        np.savetxt(outName, allData, delimiter=',', fmt='%s', header= self.header)
+        self.lines = [] # clear data
+        self.fileIndex += 1 # move to next out file index
+
+if __name__ == '__main__':
+    print('time for action')
+
+    scriptpath = os.path.dirname(__file__)
+    output = StringIO()
+    try:
+        LVMpath = pickle.load(open(scriptpath + '/cfg.p', 'rb'))
+    except:
+        print('no pickle found')
+        LVMpath = '~/Downloads'
+
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    LVMfile = askopenfilename(initialdir =  LVMpath, \
+        title='Select LVM file', \
+        filetypes = (("LVM files","*.txt"),("all files","*.*")))
+
+    if not LVMfile:
+        sys.exit()
+
+    print(LVMfile)
+    LVMpath, dummy = os.path.split(LVMfile)
+    LVMbase, dummy = os.path.splitext(LVMfile)
+    print(LVMbase)
+    pickle.dump(LVMpath, open(scriptpath + '/cfg.p', 'wb'))
+    with open(LVMfile) as infile:
+        log = logToFile(LVMbase)
+        subBlock = False
+        copy = False
+        for line in infile:
+            if line.strip() == "PULSE HISTORY":
+                copy = True
+                # ToDo get col headers 2 lines down
+                next(infile) # dummy, ignore
+                log.setHeader(next(infile))
+                continue
+            elif line.strip() == "FAULT COUNTS":
+                copy = False
+                continue
+            elif copy:
+                if line.find('C,') > 0 and subBlock == False:
+                    # start of block
+                    subBlock = True
+                    log.startLog()
+                if line.find('C') < 0 and subBlock == True:
+                    #end of block
+                    subBlock = False
+                    log.endLog()
+
+                if subBlock == True:
+                    log.addLog(line)
+
---
+++ info.md
@@ -0,0 +1,39 @@
+# Notes For Graphing Pico calibration data
+## Heading 2
+
+[Extract Values between two strings in a text file using python](https://stackoverflow.com/questions/18865058/extract-values-between-two-strings-in-a-text-file-using-python)
+
+[Filter a 2D numpy array](https://stackoverflow.com/questions/47885848/filter-a-2d-numpy-array)
+
+[Read CSV file to numpy array, first row as strings, rest as float](https://stackoverflow.com/questions/12336234/read-csv-file-to-numpy-array-first-row-as-strings-rest-as-float)
+
+[gridlines](https://www.w3schools.com/python/matplotlib_grid.asp) 
+
+[numpy read files](https://python-course.eu/numerical-programming/reading-and-writing-data-files-ndarrays.php)
+
+[multiple axis graph](https://matplotlib.org/3.4.3/gallery/ticks_and_spines/multiple_yaxis_with_spines.html)
+
+---
+## watch out for
+- pulse data is in reverse chronological order (newest first) 
+  Use **list.reverse()** (speed?)
+  <mark>Hello</mark>
+  
+---  
+## ToDo
+1. select txt file
+2. read file
+3. filter pulse list
+4. keep column names
+5. filter calibration pulses 
+   (detect multiple cals)
+6. extract relevant cols by name
+   (save result as csv)
+7. generate graph(s)
+8. save graph
+
+options:
+- view source data (table or export as csv)
+
+
+   
